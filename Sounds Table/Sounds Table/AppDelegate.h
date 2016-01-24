9153d6fc978180fe439f28b4aaeb99f2c23fb331
joshmotley <joshkmotley@gmail.com>
2016-01-23 23:15:56 -0500

    reproduce iphone sounds settings page using static cells

---
+++ Sounds Table/Sounds Table/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sounds Table
+//
+//  Created by Joshua Motley on 1/23/16.
+//  Copyright © 2016 Motley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sounds Table/Sounds Table/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sounds Table
+//
+//  Created by Joshua Motley on 1/23/16.
+//  Copyright © 2016 Motley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
